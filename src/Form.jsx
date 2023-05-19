6f51e27cf0683e1975ad99d665c7a01ba9c8cfdd
AbbasRuder <59762007+AbbasRuder@users.noreply.github.com>
2023-05-19 19:50:33 +0530

    1st - Form is not rendering properly

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,50 @@
-
+import React, { useState, useEffect } from 'react';
 
 export default function Form() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [courses, setCourses] = useState([]);
 
+
+    useEffect(() => {
+        // Fetch categories from the backend API
+        fetchCategories();
+      }, []);
+    
+      const fetchCategories = async () => {
+        try {
+          const response = await fetch('http://localhost:8000/api/categories');
+          const data = await response.json();
+          console.log(data)
+          setCategories(data);
+        } catch (error) {
+          console.error('Error fetching categories:', error);
+        }
+      };
+    
+      const fetchCourses = async () => {
+        try {
+          const response = await fetch(`http://localhost:8000/api/categories/${selectedCategory}/courses`);
+          const data = await response.json();
+          console.log(data);
+          setCourses(data);
+        } catch (error) {
+          console.error('Error fetching courses:', error);
+        }
+      };
+    
+      const handleCategoryChange = (e) => {
+        setSelectedCategory(e.target.value);
+        fetchCourses();
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        // Submit form data to the backend API
+        // Add your logic here
+      };
 
     return (
         <>
@@ -10,33 +53,54 @@ export default function Form() {
             <h1 className="text-2xl text-center font-bold mb-4 ">Contact Form</h1>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name:</label>
-                    <input className="rounded w-full py-2 px-3 text-gray-700 leading-tight" id="name" type="text" placeholder="Enter your name"/>
+                    <input className="rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                        placeholder="Enter your name"
+                        id="name"
+                        type="text"
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email:</label>
-                    <input className="rounded w-full py-2 px-3 text-gray-700 leading-tight" id="email" type="email"  placeholder="Enter your email"/>
+                    <input className="rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                        placeholder="Enter your email"
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">Category:</label>
-                    <select className="rounded w-full py-2 px-3 text-gray-700 leading-tight" id="category" >
-                    <option value="">Select a category</option>
-                    <option value="category1">Category 1</option>
-                    <option value="category2">Category 2</option>
-                    <option value="category3">Category 3</option>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">Interested Category:</label>
+                    <select className="rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                        id="category" 
+                        value={selectedCategory}
+                        onChange={handleCategoryChange}
+                    >
+                        <option value="">Select a category</option>
+                        {categories.map(category => {
+                            return <option className='text-black-100' key={category.id} value={category.id}>
+                                    {category.name}
+                                   </option>
+                        })}
                     </select>
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="course">Course:</label>
-                    <select className="rounded w-full py-2 px-3 text-gray-700 leading-tight" id="course">
-                    <option value="">Select a course</option>
-                    <option value="course1">Course 1</option>
-                    <option value="course2">Course 2</option>
-                    <option value="course3">Course 3</option>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="course">Interested Course:</label>
+                    <select className="rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                        id="course"
+                        disabled={!selectedCategory}
+                    >
+                        <option value="">Select a course</option>
+                        {courses.map(course => {
+                            return <option key={course.id} value={course.id}>{course.name}</option>
+                        })}
                     </select>
                 </div>
                 <div className="flex justify-center">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
-                    Submit
+                        Submit
                     </button>
                 </div>
             </form>
